9b6a007713b39d045220c4578b382b967cb9b0cb
Ramesh Babu Chayapathi <rem.baba@gmail.com>
2022-01-31 14:46:47 +0530

    Update jenkins-backup.sh

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -88,7 +88,7 @@ function main() {
   fi
 
   cd "${TMP_DIR}"
-  #tar -czvf "${TMP_TAR_NAME}" "${ARC_NAME}/"*
+  tar -czvf "${TMP_TAR_NAME}" "${ARC_NAME}/"*
   cd -
   mv -f "${TMP_TAR_NAME}" "${DEST_FILE}"
 
